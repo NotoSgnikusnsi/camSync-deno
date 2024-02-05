4aae3167581ed6f1af7eb82549816f244d883dcb
NotoSgnikusnsi <notosgnikusnsi@gmail.com>
2024-02-05 10:02:49 +0900

    feat: Get camera video and display it on the screen.

--- public/main.js
+++ public/main.js
@@ -1,4 +1,18 @@
-window.onload = async () => {
-  const res = await fetch('/hello');
-  document.getElementById('message').innerText = await res.text();
+window.onload = () => {
+  // Get video element
+  const video = document.getElementById("video");
+  video.autoplay = true;
+  const constraints = { audio: false, video: true };
+
+  navigator.mediaDevices
+    .getUserMedia(constraints)
+    .then((stream) => {
+      console.log("Got stream with constraints: ", constraints)
+      stream.onremovetrack = () => {
+        console.log("Stream ended.")
+      }
+      video.srcObject = stream;
+    }).catch((error) => {
+      console.error(`Error: ${error}`)
+    })
 };
